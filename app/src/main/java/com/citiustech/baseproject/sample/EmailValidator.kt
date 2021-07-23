9e13501451dfeb4b212ceb72ded04312e6651dfb
ManishKumarB <manishkumar.bhati@citiustech.com>
2021-07-23 15:30:54 +0530

    added unit test

---
+++ app/src/main/java/com/citiustech/baseproject/sample/EmailValidator.kt
@@ -0,0 +1,12 @@
+package com.citiustech.baseproject.sample
+
+object EmailValidator {
+
+    fun isValidEmail(value: String?): Boolean {
+        return true
+    }
+
+    fun isValidPassword(value: String?): Boolean {
+        return true
+    }
+}
